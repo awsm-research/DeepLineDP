341e1dd004f90eca335eb7d0735339aaab1ebb31
oathaha <windowsandandroid.app@gmail.com>
2021-09-09 17:07:31 +1000

    done code for data preprocessing

--- script/preprocess_data.py
+++ script/preprocess_data.py
@@ -1,3 +1,4 @@
+from numpy.lib.npyio import _save_dispatcher
 import pandas as pd
 import os, re
 import numpy as np
@@ -5,9 +6,14 @@ import numpy as np
 from my_util import *
 
 data_root_dir = '../datasets/original/'
+save_dir = "../datasets/parsed_data/"
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 file_lvl_dir = data_root_dir+'File-level/'
 line_lvl_dir = data_root_dir+'Line-level/'
+
 
 def is_comment_line(code_line, comments_list):
     '''
@@ -61,6 +67,8 @@ def preprocess_code_line(code_line):
     code_line = re.sub("[\\.|,|:|;|{|}|(|)]", " ", code_line)
     code_line = re.sub("<[^str][^char].*?>", "", code_line)
 
+    code_line = code_line.strip()
+
     return code_line
 
 def create_code_df(code_str, filename):
@@ -104,7 +112,13 @@ def create_code_df(code_str, filename):
         is_blank_line.append(is_empty_line(l))
         preprocess_code_lines.append(l)
 
+    if 'test' in filename:
+        is_test = True
+    else:
+        is_test = False
+
     df['filename'] = [filename]*len(code_lines)
+    df['is_test_file'] = [is_test]*len(code_lines)
     df['code_line'] = preprocess_code_lines
     df['line_number'] = np.arange(1,len(code_lines)+1)
     df['is_comment'] = is_comment
@@ -114,39 +128,50 @@ def create_code_df(code_str, filename):
 
 def preprocess_data(proj_name):
 
-    proj_name = 'activemq'
+    # proj_name = 'activemq'
     cur_all_rel = all_releases[proj_name]
 
     for rel in cur_all_rel:
         file_level_data = pd.read_csv(file_lvl_dir+rel+'_ground-truth-files_dataset.csv', encoding='latin')
         line_level_data = pd.read_csv(line_lvl_dir+rel+'_defective_lines_dataset.csv', encoding='latin')
 
+        file_level_data = file_level_data.fillna('')
+        
         buggy_files = list(line_level_data['File'].unique())
 
+        preprocessed_df_list = []
+
         for idx, row in file_level_data.iterrows():
-            # print(row)
 
             filename = row['File']
+
+            if '.java' not in filename:
+                continue
+
             code = row['SRC']
             label = row['Bug']
-            # print(type(code))
 
             code_df = create_code_df(code, filename)
             code_df['file-label'] = [label]*len(code_df)
-            code_df['line-label'] = False
+            code_df['line-label'] = [False]*len(code_df)
 
-            # print(code_df)
             # then add is_bug column here (for file-level ground truth)
 
             if filename in buggy_files:
                 buggy_lines = list(line_level_data[line_level_data['File']==filename]['Line_number'])
                 code_df['line-label'] = code_df['line_number'].isin(buggy_lines)
-                print(code_df)
-                print(filename)
-                code_df.to_csv('test.csv')
-                break
+                # print(code_df)
+                # print(filename)
+                # code_df.to_csv('test.csv')
+                # break
 
-        break
+            preprocessed_df_list.append(code_df)
+        # break
 
-preprocess_data('activemq')
+        all_df = pd.concat(preprocessed_df_list)
+        all_df.to_csv(save_dir+rel+"_with_line_ground_truth.csv",index=False)
+        print('finish release {}'.format(rel))
+
+for proj in list(all_releases.keys()):
+    preprocess_data(proj)
 
