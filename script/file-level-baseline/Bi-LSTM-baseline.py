b3dfe36a24b5cc96b011d17d175bdc36eafb1880
oathaha <windowsandandroid.app@gmail.com>
2021-09-17 11:50:31 +1000

    add code for evaluation

--- script/file-level-baseline/Bi-LSTM-baseline.py
+++ script/file-level-baseline/Bi-LSTM-baseline.py
@@ -19,8 +19,8 @@ from my_util import *
 
 arg = argparse.ArgumentParser()
 arg.add_argument('-dataset',type=str, default='activemq', help='software project name (lowercase)')
-arg.add_argument('-epochs', type=int, default=100)
-arg.add_argument('-target_epochs', type=str, default=100, help='which epoch of model to load')
+arg.add_argument('-epochs', type=int, default=20)
+arg.add_argument('-target_epochs', type=str, default='6', help='which epoch of model to load')
 arg.add_argument('-exp_name',type=str,default='')
 arg.add_argument('-train',action='store_true')
 arg.add_argument('-predict',action='store_true')
@@ -65,12 +65,13 @@ dir_suffix = dir_suffix+'-'+str(embed_dim)+'-dim'
 
 
 save_model_dir = '../../output/model/Bi-LSTM/'
-save_prediction_dir = '../../output/prediction/Bi-LSTM/'
+save_prediction_dir = '../../output/prediction/Bi-LSTM/'+dir_suffix+'/'
+
 # loss_dir = '../../output/loss/Bi-LSTM/'
 
 
-# if not os.path.exists(save_prediction_dir):
-#     os.makedirs(save_prediction_dir)
+if not os.path.exists(save_prediction_dir):
+    os.makedirs(save_prediction_dir)
 
 # if not os.path.exists(loss_dir):
 #     os.makedirs(loss_dir)
@@ -152,6 +153,21 @@ def pad_features(codevec, padding_idx, seq_length):
     
     return features
 
+def get_code_vec(code, w2v_model):
+    '''
+        input
+            code (list): a list of code string (from prepare_data_for_LSTM())
+            w2v_model (Word2Vec)
+        output
+            codevec (list): a list of token index of each file
+    '''
+    codevec = []
+
+    for c in code:
+        codevec.append([w2v_model.wv.vocab[word].index if word in w2v_model.wv.vocab else len(w2v_model.wv.vocab) for word in c.split()])
+
+    return codevec
+
 def get_dataloader_for_LSTM(w2v_model, code,encoded_labels, padding_idx):
     '''
         input
@@ -161,9 +177,10 @@ def get_dataloader_for_LSTM(w2v_model, code,encoded_labels, padding_idx):
         output
 
     '''
-    codevec = []
-    for c in code:
-        codevec.append([w2v_model.wv.vocab[word].index if word in w2v_model.wv.vocab else len(w2v_model.wv.vocab) for word in c.split()])
+    codevec = get_code_vec(code, w2v_model)
+
+    # for c in code:
+    #     codevec.append([w2v_model.wv.vocab[word].index if word in w2v_model.wv.vocab else len(w2v_model.wv.vocab) for word in c.split()])
 
     # to prevent out of memory error
     max_seq_len = min(max([len(cv) for cv in codevec]),45000)
@@ -173,6 +190,22 @@ def get_dataloader_for_LSTM(w2v_model, code,encoded_labels, padding_idx):
     dl = DataLoader(tensor_data, shuffle=True, batch_size=batch_size,drop_last=True)
 
     return dl
+
+def get_code_str(code, to_lowercase):
+    '''
+        input
+            code (list): a list of code lines from dataset
+            to_lowercase (bool)
+        output
+            code_str: a code in string format
+    '''
+
+    code_str = '\n'.join(code)
+
+    if to_lowercase:
+        code_str = code_str.lower()
+
+    return code_str
 
 def prepare_data_for_LSTM(df, to_lowercase = False):
     '''
@@ -193,10 +226,7 @@ def prepare_data_for_LSTM(df, to_lowercase = False):
 
         code = list(group_df['code_line'])
 
-        code_str = '\n'.join(code)
-
-        if to_lowercase:
-            code_str = code_str.lower()
+        code_str = get_code_str(code,to_lowercase)
 
         all_code_str.append(code_str)
 
@@ -352,19 +382,20 @@ def train_model(dataset_name):
     
 # epoch (int): which epoch to load model
 def predict_defective_files_in_releases(dataset_name, target_epochs = 100):
-
     actual_save_model_dir = save_model_dir+dataset_name+'/'
 
+    w2v_dir = get_w2v_path(include_comment=include_comment,include_test_file=include_test_file)
+        # w2v_dir = '../'+w2v_dir
+    w2v_dir = os.path.join('../'+w2v_dir,dataset_name+'-'+str(embed_dim)+'dim.bin')
+
     train_rel = all_train_releases[dataset_name]
-    eval_rel = all_eval_releases[dataset_name][1:]
+    eval_rels = all_eval_releases[dataset_name][1:]
 
-    word2vec_file_dir = os.path.join('.'+word2vec_baseline_file_dir,dataset_name+'.bin')
-
-    word2vec_model = Word2Vec.load(word2vec_file_dir)
+    word2vec_model = Word2Vec.load(w2v_dir)
     
     vocab_size = len(word2vec_model.wv.vocab) + 1
 
-    net = LSTMClassifier(batch_size, output_size, hidden_dim, vocab_size, embedding_dim)
+    net = LSTMClassifier(1, output_size, hidden_dim, vocab_size, embed_dim)
 
     checkpoint = torch.load(actual_save_model_dir+'checkpoint_'+target_epochs+'epochs.pth')
 
@@ -374,40 +405,46 @@ def predict_defective_files_in_releases(dataset_name, target_epochs = 100):
     
     net.eval()
     
-    for rel in eval_rel:
+    row_list = []
 
-        test_df = get_df_for_baseline(rel)
-        code,encoded_labels = get_data_and_label(test_df)
-        test_dl = get_dataloader_for_LSTM(word2vec_model, code,encoded_labels)
+    for rel in eval_rels:
+        test_df = get_df(rel, include_comment=include_comment, include_test_files=include_test_file, include_blank_line=include_blank_line, is_baseline=True)
 
-        y_pred = []
-        y_test = []
-        y_prob = []
+        for filename, df in tqdm(test_df.groupby('filename')):
 
-        with torch.no_grad():
-            for inputs, labels in tqdm(test_dl):
-                inputs, labels = inputs.cuda(), labels.cuda()
-                output = net(inputs)
+            file_label = bool(df['file-label'].unique())
 
-                pred = torch.round(output)
-#                     print('pred',pred)
+            code = list(df['code_line'])
 
-                y_pred.extend(pred.cpu().numpy().squeeze().tolist())
-                y_test.extend(labels.cpu().numpy().squeeze().tolist())
-                y_prob.extend(output.cpu().numpy().squeeze().tolist())
+            code_str = get_code_str(code, to_lowercase)
+            code_list = [code_str]
 
-        prediction_df = pd.DataFrame()
-        prediction_df['train_release'] = [train_rel]*len(y_test)
-        prediction_df['test_release'] = [rel]*len(y_test)
-        prediction_df['actual'] = y_test
-        prediction_df['pred'] = y_pred
-        prediction_df['prob'] = y_prob
-        
-        print('finished release',rel)
+            code_vec = get_code_vec(code_list, word2vec_model)
 
-        prediction_df.to_csv(save_prediction_dir+rel+'.csv',index=False)
+            code_tensor = torch.tensor(code_vec)
 
-    print('-'*100,'\n')
+            output = net(code_tensor)
+            file_prob = output.item()
+            prediction = bool(round(file_prob))
+
+            row_dict = {
+                        'project': dataset_name, 
+                        'train': train_rel, 
+                        'test': rel, 
+                        'filename': filename, 
+                        'file-level-ground-truth': file_label, 
+                        'prediction-prob': file_prob, 
+                        'prediction-label': prediction
+                        }
+
+            row_list.append(row_dict)
+
+            # break
+
+        break
+
+    df = pd.DataFrame(row_list)
+    df.to_csv(save_prediction_dir+rel+'-'+target_epochs+'-epochs.csv', index=False)
 
 proj_name = args.dataset
 
